a4e054c1cac5ede5f24063b712262f2ef02fbf94
fabianmagos27-glitch <fabianmagos27@gmail.com>
2026-05-03 02:18:54 +0000

    ✨feat: CSS Framework Enhance project setup with Tailwind CSS integration, update scripts, and add header styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //importar un estilo
 import './styles/main.css'
+import "./styles/header.css"
 
 //Scripts principales
 console.log("🎉 Vite + Express + Handlebards funcionionando")
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@
 import{ defineConfig } from 'vite'
 // Importo un resolvedor de rutas
 import { resolve } from "node:path"
+//import tailwind
+import tailwindcss from "@tailwindcss/vite";
 
 // Exportar una instancia de configuracion 
 export default defineConfig({
     //Directorio raiz de los archivos fuente
     root: 'src',
+    //plugins
+    plugins: [tailwindcss()],
     //Configuracion del servidor
+    //de desarrollo de front-end
     server: {
         port: 5173,
         strictPort: true    
