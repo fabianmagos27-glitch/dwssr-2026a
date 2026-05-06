f49511f17a0b721380ed6f716b73a29faeb616ae
fabianmagos27-glitch <fabianmagos27@gmail.com>
2026-05-06 15:49:27 +0000

    ✨feat: Prettier format lavaScript files and add Prettier configuration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 //importar un estilo
-import './styles/main.css'
-import "./styles/header.css"
+import './styles/main.css';
+import './styles/header.css';
 
 //Scripts principales
-console.log("🎉 Vite + Express + Handlebards funcionionando")
+console.log("🎉 Vite + Express + Handlebards funcionionando");
+let students = [
+    {
+    name: "Jhony",
+    surname: "Walker",
+},
+  {
+    name: "Sue",
+    surname: "Storm",
+},
+];
+console.log(students);
