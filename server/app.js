4bf2d5cc1649c812bfe766915cb604ded49178f4
fabianmagos27-glitch <fabianmagos27@gmail.com>
2026-05-12 23:09:50 +0000

    ✨Winston Logger

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-//importando winston logger
-import logger from './lib/winston.js';
+//importando winston Logger
+import Logger from './lib/winston.js';
 import hbs from 'hbs';
 
 
@@ -15,13 +15,18 @@ import usersRouter from '#routes/users.js';
 import authorRouter from '#routes/author.js';
 //Importando el registradoe de helpers
 import { registerViteHelper } from './lib/vite.js';
+import logger from './lib/winston.js';
 
 //import app from '../app.js';
 var app = express();
+logger.info("Creando la instacia de express");
+logger.info("Se inicia con la instacia");
+
 
 //Recreando variable de path
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// Asegúrate de que sean DOS guiones bajos: __filename
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,25 +34,24 @@ app.set('view engine', 'hbs');
 
 //Registrando Helpers para el Engine
 registerViteHelper(hbs)
-
-//Redirigiendo el flujo del logs de morgan 
+//Redirigiendo el flujo de logs de morgan
 //a winston
-//mogan---->[logs]---->Winston---->transportes
+//morgan ya genera ->[logs]--> Winston-->transportes
 app.use(morgan('dev',{
-  stream:{
-   write: (msg)=> logger.http(msg.trim())
+  stream: {
+    write: (msg) => Logger.http(msg.trim())
   }
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//Archivos estaticos
-app.use(express.static(path.join(__dirname, '../public')));
-//Archivos estativos de vite
-if (process.env.NODE_ENV  === "production" ){
+//Archivos estaticos de vite
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname,'..','dist')));
 }
-//Archivos estativos del backend
+// Archivos estaticos del backend
+app.use(express.static(path.join(__dirname,'..','public')));
+app.use(express.static(path.join(__dirname, '../public')));
 console.log("Ruta:  "+  path.join(__dirname, 'public'));
 
 //registrando las rutas a los enrutadores
@@ -57,12 +61,14 @@ app.use('/author', authorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  logger.warn('Se consulto la ruta no encontrada${req.originalUrl}');
   next(createError(404));
 });
 
 // error handler
 //eslint-disable-next-line no-unused-vars
 app.use(function(err, req, res, next) {
+  logger.error(`Error ${err.status || 500} -> $(err.message)`);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
